0def76824efacb0cd981c9a15b2126683170a72e
Fathima <shifasalam036@gmail.com>
2024-07-02 10:22:06 +0530

    context

--- tours-and-travels/src/Context Api/Contextapi.jsx
+++ tours-and-travels/src/Context Api/Contextapi.jsx
@@ -3,21 +3,17 @@ import React, { createContext, useState } from 'react'
 
 export const addTourResponseContext = createContext()
 export const editTourResponseContext = createContext()
-// export const addReviewResponseContext = createContext()
 
 function Contextapi({ children }) {
 
     const [addTourResponse, setAddTourResponse] = useState("")
     const [editTourResponse, setEditTourResponse] = useState("")
-    // const [addReviewResponse, setAddReviewResponse] = useState("")
     return (
         <>
             <addTourResponseContext.Provider value={{ addTourResponse, setAddTourResponse }}>
-                {/* <addReviewResponseContext.Provider value={{addReviewResponse,setAddReviewResponse}}> */}
                     <editTourResponseContext.Provider value={{ editTourResponse, setEditTourResponse }}>
                         {children}
                     </editTourResponseContext.Provider>
-                {/* </addReviewResponseContext.Provider> */}
             </addTourResponseContext.Provider>
 
         </>
